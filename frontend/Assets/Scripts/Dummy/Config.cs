a3d0f578c6da42a54a31dbe61ff37b75ae5e253c
Dario D. Müller <dario-domi@hotmail.de>
2019-09-08 00:53:08 +0200

    added dynamic card values

--- frontend/Assets/Scripts/Dummy/Config.cs
+++ frontend/Assets/Scripts/Dummy/Config.cs
@@ -8,6 +8,7 @@ public class Config : MonoSingleton<Config>
 {
     public Vector2 BaseScreenSize;
     public string Url = "https://evan.dtrautwein.eu/api";
+    public List<Sprite> CardImages;
 
 
 }
--- frontend/Assets/Scripts/Quartett/Models/CardDto.cs
+++ frontend/Assets/Scripts/Quartett/Models/CardDto.cs
@@ -2,17 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class CardDto : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public int Id;
+    public string Headline;
+    public string Attr1;
+    public string Attr2;
+    public string Attr3;
+    public string Attr4;
+    public string Attr5;
 }
