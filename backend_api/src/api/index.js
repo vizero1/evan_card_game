2e5de5fc82425cd3474cb7a122e0dbd1b55c77cc
Dennis Trautwein <dennis.trautwein@freiheit.com>
2019-09-08 11:15:57 +0200

    Change datastructures

--- backend_api/src/api/index.js
+++ backend_api/src/api/index.js
@@ -1,7 +1,15 @@
 const { DigitalTwin, Container } = require('@evan.network/api-blockchain-core');
+import _ from 'lodash';
 import { version } from '../../package.json';
 import { Router } from 'express';
 import facets from './facets';
+import {
+  createDigitalTwin,
+  configurePlugin,
+  createCardsContainer,
+  loadAllCardAddresses,
+  playerContainersForGameId
+} from './utils';
 
 const cardDeckTwinAddress = '0xC12748b85e24529FF691CCd0c902aDB9232897E5';
 
@@ -20,161 +28,40 @@ export default ({ config, runtime }) => {
     try {
       const { playerName } = req.body;
 
-      console.log('Creating game for player ' + playerName);
+      console.log('Creating digitalTwin for player:', playerName);
+      const game = await createDigitalTwin(runtime);
+      const gameAddress = await game.getContractAddress();
+      console.log('Created new digital twin with address:', gameAddress);
 
-      // create a new twin with a description
-      const description = {
-        name: 'New crypto quartet game',
-        description: 'evan.network crypto quartet card game',
-        author: 'magic (ProofOfMoin Hackathon Group)',
-        version: '0.0.1',
-        dbcpVersion: 2
-      };
+      const plugin = configurePlugin();
 
-      const game = await DigitalTwin.create(runtime, {
-        accountId: runtime.activeAccount,
-        description
-      });
-      console.log(
-        `Created new digital twin with address: "${await game.getContractAddress()}"`
-      );
+      const [player1cards, player2cards] = await Promise.all([
+        createCardsContainer(game, plugin, 'player1cards', playerName, true),
+        createCardsContainer(game, plugin, 'player2cards', '', false)
+      ]);
 
-      const plugin = JSON.parse(JSON.stringify(Container.plugins.metadata));
+      console.log('Setting username of player1 to:', playerName);
+      await player1cards.setEntry('username', playerName);
 
-      plugin.template.properties.cards = {
-        dataSchema: {
-          type: 'array',
-          items: {
-            type: 'object',
-            properties: {
-              index: {
-                type: 'integer'
-              },
-              address: {
-                type: 'string'
-              }
-            }
-          }
-        },
-        permissions: { 0: ['set'] },
-        type: 'list'
-      };
+      console.log('Setting hasTurn of player1 to:', true);
+      await player1cards.setEntry('hasTurn', true);
 
-      const cardListDescription = {
-        name: 'quartetGameContainer',
-        description: 'quartetGameContainer description',
-        author: 'magic (ProofOfMoin Hackathon Group)',
-        version: '0.0.1',
-        dbcpVersion: 2
-      };
+      console.log('Setting username of 2 to:', '');
+      await player2cards.setEntry('username', '');
 
-      console.log('Create player1cards container');
-      // create a container with default template
-      //const { player1cards, player2cards, cardDeckAddresses, gameLogs } = await game.createContainers({
-      const { player1cards } = await game.createContainers({
-        player1cards: { plugin, description: cardListDescription } // use later { plugin }
-      });
-      console.log(
-        'Created player1cards container with address:',
-        await player1cards.getContractAddress()
-      );
+      console.log('Setting hasTurn of 2 to:', false);
+      await player2cards.setEntry('hasTurn', false);
 
-      console.log('Set properties for player1cards');
-      player1cards.setEntry('username', playerName);
-      player1cards.setEntry('hasTurn', true);
+      console.log('Setting entrie Done!');
+      var cards = await loadAllCardAddresses(runtime);
 
-      console.log('Create player2cards container');
-      const { player2cards } = await game.createContainers({
-        player2cards: { plugin, description: cardListDescription } // use later { plugin }
-      });
-      console.log(
-        'Created player2cards container with address:',
-        await player2cards.getContractAddress()
-      );
-      player2cards.setEntry('username', '');
-      player2cards.setEntry('hasTurn', false);
+      console.log('Adding cards to player 1');
+      const cardsForPlayer1 = cards.splice(0, cards.length / 2);
+      await player1cards.addListEntries('cards', cardsForPlayer1);
 
-      console.log('Create cardDeckAddresses container');
-      const { cardDeckAddresses } = await game.createContainers({
-        cardDeckAddresses: {} //use later: {plugin}
-      });
-
-      console.log('Create gameLogs container');
-      const { gameLogs } = await game.createContainers({
-        gameLogs: {}
-      });
-      console.log(
-        'Created gameLogs with address',
-        await gameLogs.getContractAddress()
-      );
-
-      //load all cards
-      const cardDeckInstance = new DigitalTwin(runtime, {
-        accountId: runtime.activeAccount,
-        address: cardDeckTwinAddress
-      });
-      console.log('Fetching cards...');
-      const cardsObject = await cardDeckInstance.getEntries();
-
-      // const cards = [];
-      // const cardNames = Object.keys(cardsObject);
-      // for (let i = 0; i < cardNames.length; i++) {
-      //   cards.push(await cardsObject[cardNames[i]].value.getContractAddress());
-      // }
-
-      const cards = await Promise.all(
-        Object.keys(cardsObject).map(async (key, index) => {
-          return {
-            index: index,
-            address: await cardsObject[key].value.getContractAddress()
-          };
-        })
-      );
-
-      console.log('Shuffling cards...');
-      // shuffle cards
-      let shuffleCounter = cards.length;
-
-      // While there are elements in the array
-      while (shuffleCounter > 0) {
-        // Pick a random index
-        let index = Math.floor(Math.random() * shuffleCounter);
-
-        // Decrease counter by 1
-        shuffleCounter--;
-
-        // And swap the last element with it
-        let temp = cards[shuffleCounter];
-        cards[shuffleCounter] = cards[index];
-        cards[index] = temp;
-      }
-
-      console.log('Add first card to player 1');
-      const card1 = cards.splice(0, 1);
-      console.log('card1', card1);
-      await player1cards.addListEntries('cards', card1);
-      console.log('player1cards', await player1cards.getListEntries('cards'));
-
-      console.log('Add second card to player 2');
-      const card2 = cards.splice(0, 1);
+      console.log('Adding cards to player 2');
+      const card2 = cards;
       await player2cards.addListEntries('cards', card2);
-
-      console.log('Add remaining cards to deck');
-      await cardDeckAddresses.addListEntries('cards', cards);
-
-      // //give access to container
-      // await player1cards.shareProperties([
-      //   { accountId: player1ID, read: ['cards'] }
-      // ]);
-
-      // await player2cards.shareProperties([
-      //   { accountId: player2ID, read: ['cards'] }
-      // ]);
-
-      // await gameLogs.shareProperties([
-      //   { accountId: player1ID, write: ['moves'] },
-      //   { accountId: player2ID, write: ['moves'] }
-      // ]);
 
       res.json({
         playerId: await player1cards.getContractAddress(),
@@ -192,49 +79,47 @@ export default ({ config, runtime }) => {
       const { gameId } = req.params;
       const { playerName } = req.body;
 
-      console.log('Getting DigitalTwin for gameId', gameId);
+      console.log(`${playerName} wants to join game ${gameId}`);
 
-      const digitalTwin = new DigitalTwin(runtime, {
-        accountId: runtime.activeAccount,
-        address: gameId
-      });
+      const {
+        player1container,
+        player2container
+      } = await playerContainersForGameId(runtime, gameId);
 
-      console.log('Getting entries of DigitalTwin');
-      const containers = await digitalTwin.getEntries();
+      console.warn(await player1container.getDescription());
 
-      const { player1cards, player2cards } = containers;
-      const player1 = player1cards.value;
-      const player2 = player2cards.value;
+      const usernamePlayer1 = await player1container.getEntry('username');
 
-      const usernamePlayer1 = await player1.getEntry('username');
-      console.log('usernamePlayer1:', usernamePlayer1, '==', playerName, '?');
+      console.log('Is playerName player1?');
       if (usernamePlayer1 == playerName) {
         console.log(usernamePlayer1, '==', playerName, '!');
         res.json({
-          playerId: await player1.getContractAddress()
+          playerId: await player1container.getContractAddress()
         });
         return;
       }
 
-      const usernamePlayer2 = await player2.getEntry('username');
-      console.log(usernamePlayer2, '==', playerName, '?');
+      const usernamePlayer2 = await player2container.getEntry('username');
+      console.log('Is playerName player2?');
       if (usernamePlayer2 == playerName) {
         console.log(usernamePlayer2, '==', playerName, '!');
         res.json({
-          playerId: await player2.getContractAddress()
+          playerId: await player2container.getContractAddress()
         });
         return;
       }
 
+      console.log('Is player2 set?');
       if (usernamePlayer2 != '') {
+        console.log('Yes');
         res.sendStatus(403);
         return;
       }
 
-      await player2.setEntry('username', playerName);
+      await player2container.setEntry('username', playerName);
 
       res.json({
-        playerId: await player2.getContractAddress()
+        playerId: await player2container.getContractAddress()
       });
     } catch (error) {
       console.error(error);
@@ -248,58 +133,67 @@ export default ({ config, runtime }) => {
       const { gameId } = req.params;
       const { playerId, attribute } = req.body;
 
-      console.log('Getting DigitalTwin for gameId', gameId);
+      const {
+        player1container,
+        player2container
+      } = await playerContainersForGameId(runtime, gameId);
 
-      const digitalTwin = new DigitalTwin(runtime, {
-        accountId: runtime.activeAccount,
-        address: gameId
-      });
-
-      console.log('Getting entries of DigitalTwin');
-      const containers = await digitalTwin.getEntries();
-
-      const { player1cards, player2cards } = containers;
-      const player1 = player1cards.value;
-      const player2 = player2cards.value;
-
-      const player1Address = await player1.getContractAddress();
-      const player2Address = await player2.getContractAddress();
+      const player1Address = await player1container.getContractAddress();
+      const player2Address = await player2container.getContractAddress();
 
       var myCards;
       var opponentCards;
       if (player1Address == playerId) {
-        myCards = player1cards;
-        opponentCards = player2cards;
+        myCards = player1container;
+        opponentCards = player2container;
       } else if (player2Address == playerId) {
-        myCards = player2cards;
-        opponentCards = player1cards;
+        myCards = player2container;
+        opponentCards = player1container;
       } else {
         res.sendStatus(403);
         return;
       }
 
-      var myCardsAddresses = await myCards.value.getListEntries('cards');
-      var opponentCardsAddresses = await opponentCards.value.getListEntries(
-        'cards'
+      var [
+        myCardsAddressesOriginal,
+        opponentCardsAddressesOriginal
+      ] = await Promise.all([
+        myCards.getListEntries('cards'),
+        opponentCards.getListEntries('cards')
+      ]);
+
+      const myCardsAddresses = _.sortBy(myCardsAddressesOriginal, 'index');
+      const opponentCardsAddresses = _.sortBy(
+        opponentCardsAddressesOriginal,
+        'index'
       );
 
       const myCurrentCardContainer = new Container(runtime, {
         accountId: runtime.activeAccount,
-        address: myCardsAddresses[0]
+        address: myCardsAddresses[0].address
       });
 
       const opponentCurrentCardContainer = new Container(runtime, {
         accountId: runtime.activeAccount,
-        address: opponentCardsAddresses[0]
+        address: opponentCardsAddresses[0].address
       });
 
+      const myCurrentCardOriginalIndex = _.findIndex(myCardsAddressesOriginal, {
+        address: await myCurrentCardContainer.getContractAddress()
+      });
+
+      const opponentCurrentCardOriginalIndex = _.findIndex(
+        opponentCardsAddressesOriginal,
+        {
+          address: await opponentCurrentCardContainer.getContractAddress()
+        }
+      );
+
       console.log('Fetching cards...');
-      const myCurrentCard = await myCurrentCardContainer.getEntry(
-        'characteristics'
-      );
-      const opponentCurrentCard = await opponentCurrentCardContainer.getEntry(
-        'characteristics'
-      );
+      const [myCurrentCard, opponentCurrentCard] = await Promise.all([
+        myCurrentCardContainer.getEntry('characteristics'),
+        opponentCurrentCardContainer.getEntry('characteristics')
+      ]);
 
       const myValue = myCurrentCard[attribute];
       const opponentValue = myCurrentCard[attribute];
@@ -320,27 +214,41 @@ export default ({ config, runtime }) => {
       }
 
       if (hasWon) {
-        const movedCard = opponentCardsAddresses.splice(0, 1);
-        console.log('I have won against card', movedCard);
+        // const movedCard = opponentCardsAddresses.splice(0, 1);
+        console.log('I have won against card'); //, movedCard.address);
 
+        console.log(
+          'Remove card at index',
+          myCurrentCardOriginalIndex,
+          'from opponent deck'
+        );
         await runtime.dataContract.removeListEntry(
-          await opponentCards.value.getContractAddress(),
+          await opponentCards.getContractAddress(),
           'cards',
-          0
+          myCurrentCardOriginalIndex
         );
 
-        console.log('Writing new card addresses list for opponent');
-        await opponentCards.value.addListEntries(
+        // console.log('Writing new card addresses list for opponent');
+        // await opponentCards.value.addListEntries(
+        //   'cards',
+        //   opponentCardsAddresses
+        // );
+
+        // const myPlayedCard = myCardsAddresses.splice(0, 1);
+
+        console.log(
+          'Remove card at index',
+          myCurrentCardOriginalIndex,
+          'from my deck'
+        );
+        await runtime.dataContract.removeListEntry(
+          await myCards.getContractAddress(),
           'cards',
-          opponentCardsAddresses
+          myCurrentCardOriginalIndex
         );
 
-        const myPlayedCard = myCardsAddresses.splice(0, 1);
-        myCardsAddresses.push(myPlayedCard);
-        myCardsAddresses.push(movedCard);
-
-        console.log('Writing new card addresses list for me');
-        await myCards.value.addListEntries('cards', myCardsAddresses);
+        // console.log('Writing new card addresses list for me');
+        // await myCards.value.addListEntries('cards', myCardsAddresses);
       } else {
         const movedCard = myPlayedCardAddresses.splice(0, 1);
         await myPlayedCard.value.addListEntries('cards', myPlayedCardAddresses);
@@ -370,56 +278,47 @@ export default ({ config, runtime }) => {
       const { gameId } = req.params;
       const { playerId } = req.query;
 
-      console.log('Getting DigitalTwin for gameId', gameId);
+      const {
+        player1container,
+        player2container
+      } = await playerContainersForGameId(runtime, gameId);
 
-      const digitalTwin = new DigitalTwin(runtime, {
-        accountId: runtime.activeAccount,
-        address: gameId
-      });
-
-      console.log('Getting entries of DigitalTwin');
-      const containers = await digitalTwin.getEntries();
-
-      const { player1cards, player2cards } = containers;
-      const player1 = player1cards.value;
-      const player2 = player2cards.value;
-
-      const player1Address = await player1.getContractAddress();
-      const player2Address = await player2.getContractAddress();
+      const player1Address = await player1container.getContractAddress();
+      const player2Address = await player2container.getContractAddress();
 
       var myCards;
       var opponentCards;
       if (player1Address == playerId) {
-        myCards = player1cards;
-        opponentCards = player2cards;
+        myCards = player1container;
+        opponentCards = player2container;
       } else if (player2Address == playerId) {
-        myCards = player2cards;
-        opponentCards = player1cards;
+        myCards = player2container;
+        opponentCards = player1container;
       } else {
         res.sendStatus(403);
         return;
       }
 
-      const opponent = (await opponentCards.value.getEntry('username')) != '';
-      const myTurn = await myCards.value.getEntry('hasTurn');
-      const cards = await myCards.value.getListEntries('cards');
+      const opponent = (await opponentCards.getEntry('username')) != '';
+      const myTurn = await myCards.getEntry('hasTurn');
+      var cards = await myCards.getListEntries('cards');
 
-      var card = {};
-      if (cards.length > 0) {
-        const cardDeckInstance = new Container(runtime, {
-          accountId: runtime.activeAccount,
-          address: cards[0]
-        });
+      cards.sort((a, b) => {
+        return a.index - b.index;
+      });
 
-        console.log('Fetching cards...');
-        card = await cardDeckInstance.getEntry('characteristics');
-      }
+      const cardDeckInstance = new Container(runtime, {
+        accountId: runtime.activeAccount,
+        address: cards[0].address
+      });
+
+      console.log('Fetching cards...');
+      const card = await cardDeckInstance.getEntry('characteristics');
 
       res.json({
         hasOpponent: opponent,
         myTurn: myTurn,
-        card: card,
-        attributeLastRound: ''
+        card: card
       });
     } catch (error) {
       console.error(error);
---
+++ backend_api/src/api/utils.js
@@ -0,0 +1,115 @@
+import _ from 'lodash';
+
+const { DigitalTwin, Container } = require('@evan.network/api-blockchain-core');
+
+const cardDeckTwinAddress = '0xC12748b85e24529FF691CCd0c902aDB9232897E5';
+
+export const createDigitalTwin = async runtime => {
+  // create a new twin with a description
+  const description = {
+    name: 'New crypto quartet game',
+    description: 'evan.network crypto quartet card game',
+    author: 'magic (ProofOfMoin Hackathon Group)',
+    version: '0.0.1',
+    dbcpVersion: 2
+  };
+
+  return await DigitalTwin.create(runtime, {
+    accountId: runtime.activeAccount,
+    description
+  });
+};
+
+export const createCardsContainer = async (digitalTwin, plugin, name) => {
+  console.log('Creating cards container for', name);
+
+  const cardListDescription = {
+    name: 'quartetGameContainer',
+    description: 'quartetGameContainer description',
+    author: 'magic (ProofOfMoin Hackathon Group)',
+    version: '0.0.1',
+    dbcpVersion: 2
+  };
+
+  const container = await digitalTwin.createContainers({
+    [name]: { plugin, description: cardListDescription }
+  });
+
+  console.log(
+    'Created cards container for container ' + name + 'with address:',
+    await container[name].getContractAddress()
+  );
+
+  return container[name];
+};
+
+export const configurePlugin = () => {
+  const plugin = JSON.parse(JSON.stringify(Container.plugins.metadata));
+
+  plugin.template.properties.cards = {
+    dataSchema: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          index: {
+            type: 'integer'
+          },
+          address: {
+            type: 'string'
+          }
+        }
+      }
+    },
+    permissions: { 0: ['set'] },
+    type: 'list'
+  };
+
+  return plugin;
+};
+
+export const loadAllCardAddresses = async runtime => {
+  console.log('Get digital twin for all cards', cardDeckTwinAddress);
+  const cardDeckInstance = new DigitalTwin(runtime, {
+    accountId: runtime.activeAccount,
+    address: cardDeckTwinAddress
+  });
+
+  console.log('Loading all cards...');
+  const cardsObject = await cardDeckInstance.getEntries();
+
+  console.log('Shuffling cards...');
+  const cardKeys = _.shuffle(Object.keys(cardsObject));
+
+  const cards = await Promise.all(
+    cardKeys.map(async (key, index) => {
+      return {
+        index: index,
+        address: await cardsObject[key].value.getContractAddress()
+      };
+    })
+  );
+  console.log('Loaded cards', cards);
+  return cards;
+};
+
+export const digitalTwinForGameId = (runtime, gameId) => {
+  console.log('Getting DigitalTwin for gameId', gameId);
+
+  return new DigitalTwin(runtime, {
+    accountId: runtime.activeAccount,
+    address: gameId
+  });
+};
+
+export const playerContainersForGameId = async (runtime, gameId) => {
+  const digitalTwin = digitalTwinForGameId(runtime, gameId);
+
+  const containers = await digitalTwin.getEntries();
+  const { player1cards, player2cards } = containers;
+
+  return {
+    player1container: player1cards.value,
+    player2container: player2cards.value
+  };
+};
